00d7ad32170c134f1206ee67bfc6313aed3b794f
Smarika Tripathi <smarikatripathi1@gmail.com>
2024-12-27 16:22:57 +0545

    step6and7

---
+++ lambda function/step6.py
@@ -0,0 +1,10 @@
+#Using insert(), add 1 to my_list in the proper position so that it is counting upward, then print the list.
+my_list = [1, 2]
+my_list.insert(1,1)
+my_list.append(3)
+print(my_list)
+
+print(my_list[0])
+
+my_list[0] = 0
+print(my_list)
---
+++ lambda function/step7.py
@@ -0,0 +1,13 @@
+#Use pop() to remove the last element from my_list, then print my_list
+my_list = [1, 2]
+
+my_list.append(3)
+print(my_list)
+my_list.pop()
+print(my_list[0])
+
+my_list[0] = 0
+print(my_list)
+
+my_list.insert(1, 1)
+print(my_list)
