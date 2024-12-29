b96871f96987d86b25703597c8e36d2476a6cea0
Smarika Tripathi <smarikatripathi1@gmail.com>
2024-12-29 22:25:21 +0545

    28

---
+++ lambda function/step28.py
@@ -0,0 +1,4 @@
+#Next, define a function named filter_expenses_by_category that takes two parameters: expenses and category. Use pass to fill the function body.
+
+def filter_expenses_by_category(expenses, category):
+    pass
