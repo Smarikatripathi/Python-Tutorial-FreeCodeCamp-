121b73079081084e94145ca6d71668a4ef0915b4
Smarika Tripathi <smarikatripathi1@gmail.com>
2024-12-31 15:02:15 +0545

    33

---
+++ lambda function/step33.py
@@ -0,0 +1,6 @@
+#Below the expenses list, create a while loop. Use True for the condition, and print the string '\nExpense Tracker' inside the loop body to show the title of the program.
+
+def main():
+    expenses = []
+    while True:
+        print('\nExpense Tracker')
