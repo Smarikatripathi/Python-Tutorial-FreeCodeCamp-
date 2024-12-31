5b06670eb7dd49d42c3dfd2de7e679d9f2b2920a
Smarika Tripathi <smarikatripathi1@gmail.com>
2024-12-31 15:07:16 +0545

    36

---
+++ lambda function/step36.py
@@ -0,0 +1,9 @@
+#Provide the other menu options by printing the following three strings in your while loop: '3. Show total expenses', '4. Filter expenses by category', and '5. Exit'. Keep adding the print() calls in order.
+
+while True:
+        print('\nExpense Tracker')
+        print('1. Add an expense')
+        print('2. List all expenses')
+        print('3. Show total expenses')
+        print('4. Filter expenses by category')
+        print('5. Exit')
