38cac1964946e2f4a73fe6ff8bbb99e32a75942e
Smarika Tripathi <smarikatripathi1@gmail.com>
2024-12-31 15:01:36 +0545

    32

---
+++ lambda function/step32.py
@@ -0,0 +1,5 @@
+#The next step is to define the main function, which will be the entry point of the interactive expense tracker program.
+#Define a function named main without parameters. Fill the function body with the expenses list you created at the beginning of this project. You will use this list to store the expense records.
+
+def main():
+    expenses = []
