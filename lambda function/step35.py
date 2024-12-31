4faaf19265310585417188b512852c32c66110d6
Smarika Tripathi <smarikatripathi1@gmail.com>
2024-12-31 15:05:22 +0545

    35

---
+++ lambda function/step35.py
@@ -0,0 +1,6 @@
+#Next, add another print() call and pass the string '2. List all expenses'.
+
+while True:
+    print('\nExpense Tracker')
+    print('1. Add an expense')
+    print('2. List all expenses')
