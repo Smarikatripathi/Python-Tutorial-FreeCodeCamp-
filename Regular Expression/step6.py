99acf84859dfcd97d9b48a70ef2d1eb6fce15c25
Smarika Tripathi <smarikatripathi1@gmail.com>
2025-01-01 21:44:03 +0545

    6

---
+++ Regular Expression/step6.py
@@ -0,0 +1,10 @@
+#Just before it, add a comment saying Combine all characters.
+
+import string
+# Define the possible characters for the password
+letters = string.ascii_letters
+digits = string.digits
+symbols = string.punctuation
+
+#Combine all characters
+all_characters = letters + digits + symbols
