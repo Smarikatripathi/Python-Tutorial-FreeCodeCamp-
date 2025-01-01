13f9c70f8e4167812a0cff54781d2185a10dac3f
Smarika Tripathi <smarikatripathi1@gmail.com>
2025-01-01 21:42:06 +0545

    3

---
+++ Regular Expression/step3.py
@@ -0,0 +1,6 @@
+#Declare two new variables named digits and symbols and assign them string.digits and string.punctuation, respectively.
+
+import string
+letters = string.ascii_letters
+digits = string.digits
+symbols = string.punctuation
