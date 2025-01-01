07f69c483536dc9ddf47ba4eb31c836d3ebb651a
Smarika Tripathi <smarikatripathi1@gmail.com>
2025-01-01 21:42:34 +0545

    4

---
+++ Regular Expression/step4.py
@@ -0,0 +1,7 @@
+#Just before them, add a comment saying Define the possible characters for the password.
+
+import string
+#Define the possible characters for the password
+letters = string.ascii_letters
+digits = string.digits
+symbols = string.punctuation
