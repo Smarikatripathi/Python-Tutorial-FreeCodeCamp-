b6c4adcd333a254ad41846cb747390168866b6d2
Smarika Tripathi <smarikatripathi1@gmail.com>
2025-01-03 20:20:46 +0545

    14

---
+++ Regular Expression/step13.py
@@ -0,0 +1,11 @@
+#Declare a generate_password function and write all your code except the import lines inside the function body.
+import secrets
+import string
+def generate_password ():
+# Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+ 
+# Combine all characters
+    all_characters = letters + digits + symbols
