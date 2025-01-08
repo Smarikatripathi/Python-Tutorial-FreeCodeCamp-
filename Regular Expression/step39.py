f526c62b7af6189768a4b51610facf9878f903aa
Smarika Tripathi <smarikatripathi1@gmail.com>
2025-01-08 21:38:21 +0545

    39

---
+++ Regular Expression/step39.py
@@ -0,0 +1,6 @@
+#Add a new tuple to the constraints list. Use lowercase as the first item and a regex pattern that matches a single lowercase letter as the second item.
+
+constraints = [
+            (nums, '[0-9]'),
+            (lowercase, '[a-z]')
+        ] 
