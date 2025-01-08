b17ae736dd0505c561d9733de9697661d4d338c5
Smarika Tripathi <smarikatripathi1@gmail.com>
2025-01-08 21:38:53 +0545

    40

---
+++ Regular Expression/step40.py
@@ -0,0 +1,7 @@
+#Add a third tuple to the constraints list. Use the uppercase parameter as the first item and a regex pattern that matches a single uppercase letter as the second item.
+
+constraints = [
+            (nums, '[0-9]'),
+            (lowercase, '[a-z]'),
+            (uppercase, '[A-Z]')
+        ]  
