9bc7453b3c202d516a115c424b6ce433fc19f6ee
Smarika Tripathi <smarikatripathi1@gmail.com>
2025-01-09 20:01:19 +0545

    45

---
+++ Regular Expression/step45.py
@@ -0,0 +1,8 @@
+#Now, turn the four patterns from the constraints list into raw strings.
+
+constraints = [
+            (nums, r'[0-9]'),
+            (lowercase, r'[a-z]'),
+            (uppercase, r'[A-Z]'),
+            (special_chars, r'')
+        ]     
