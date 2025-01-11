392d85572cb928593385a0a7a9c2654b3d568e70
Smarika Tripathi <smarikatripathi1@gmail.com>
2025-01-11 20:27:23 +0545

    52

---
+++ Regular Expression/step52.py
@@ -0,0 +1,3 @@
+#Now, combine your raw string with an f-string and interpolate your symbols variable inside the character class. Remember that you can interpolate a variable within an f-string using curly brackets { }.
+
+ (special_chars, fr'[{symbols}]')
