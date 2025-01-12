a1d2131b7446413b3ea89df62de62cbe9ebc18a9
Smarika Tripathi <smarikatripathi1@gmail.com>
2025-01-12 08:20:56 +0545

    54

---
+++ Regular Expression/step54.py
@@ -0,0 +1,5 @@
+#After your new comment, write a for loop to iterate over the constraints list. Use constraint and pattern as the loop variables.
+
+for constraint,pattern in constraints:
+    pass
+        
