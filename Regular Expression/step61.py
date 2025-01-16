800bb194509de7adb3f5c87bbf77b2e33f957fa0
Smarika Tripathi <smarikatripathi1@gmail.com>
2025-01-16 22:49:08 +0545

    61

---
+++ Regular Expression/step61.py
@@ -0,0 +1,3 @@
+#Replace your existing for loop and two if statements with a single if statement. For the if condition, use a call to the all() function and pass an empty list as the argument to the function call.
+
+        if all([]):
