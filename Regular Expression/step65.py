7e9b9be1dce1aa07f0fe377067562d2b459240ff
Smarika Tripathi <smarikatripathi1@gmail.com>
2025-01-17 09:29:15 +0545

    65

---
+++ Regular Expression/step65.py
@@ -0,0 +1,5 @@
+#Now it's time to test your function. Uncomment the last two lines in your code and modify the function call passing 5 arguments. Use 8 for the length and 1 for the other four constraints.
+
+
+new_password = generate_password(8,1,1,1,1)
+print(new_password)
