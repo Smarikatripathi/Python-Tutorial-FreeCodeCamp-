29b03d9175d16aedf2360751aa416aa38e5f53ad
Smarika Tripathi <smarikatripathi1@gmail.com>
2025-01-18 17:21:00 +0545

    68

---
+++ Regular Expression/step68.py
@@ -0,0 +1,3 @@
+#Modify your function declaration to take default parameters. Use 16 for the length and 1 for the other constraints.
+
+def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase=1):
