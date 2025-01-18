400bef9eeef53c3b8d585760333b16bf4f31659e
Smarika Tripathi <smarikatripathi1@gmail.com>
2025-01-18 17:20:33 +0545

    67

---
+++ Regular Expression/step67.py
@@ -0,0 +1,4 @@
+#To confirm this, try to change the order of length=8 and nums=1 in your function call.
+
+new_password = generate_password(nums=1,length=8, special_chars=1, uppercase=1, lowercase=1)
+print(new_password)
