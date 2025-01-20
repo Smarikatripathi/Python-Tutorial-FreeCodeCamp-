0c1d1ab30057c66d6bc4214c6e94cf0fa4a5b386
Smarika Tripathi <smarikatripathi1@gmail.com>
2025-01-20 22:26:39 +0545

    70

---
+++ Regular Expression/step69.py
@@ -0,0 +1,4 @@
+#Modify your generate_password() call to take only length=8.
+
+new_password = generate_password(length=8)
+print(new_password)
---
+++ Regular Expression/step70.py
@@ -0,0 +1,6 @@
+
+#Now, remove all the arguments from your function call.
+
+
+new_password = generate_password(length=8)
+print(new_password)
